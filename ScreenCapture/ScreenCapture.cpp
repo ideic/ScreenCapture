4da663ea416a1491d82a67c1966f94c1b39649e8
Csaba Idei <csaba.idei@tcandc.com>
2019-01-31 17:08:27 +0100

    h264 looks ok

--- ScreenCapture/ScreenCapture.cpp
+++ ScreenCapture/ScreenCapture.cpp
@@ -65,17 +65,10 @@ void ConfigureVideoParams(mfxVideoParam & conf) {
 void ConfigureEncodeParams(mfxVideoParam & conf) {
 
 	memset(&conf, 0, sizeof(conf));
-	//conf.IOPattern = MFX_IOPATTERN_IN_VIDEO_MEMORY;
-	//conf.mfx.CodecId = MFX_CODEC_MPEG2;
-	//conf.mfx.FrameInfo.FourCC = MFX_FOURCC_NV12;
-	//conf.mfx.FrameInfo.ChromaFormat = MFX_CHROMAFORMAT_YUV420;
-	//conf.mfx.FrameInfo.Width = conf.mfx.FrameInfo.CropW = 1920;
-	//conf.mfx.FrameInfo.Height = 1088;
-	//conf.mfx.FrameInfo.CropH = 1080;
 
 	conf.IOPattern = MFX_IOPATTERN_IN_SYSTEM_MEMORY;
 	conf.mfx.CodecId = MFX_CODEC_AVC;
-	conf.mfx.TargetUsage = MFX_TARGETUSAGE_BALANCED;
+	conf.mfx.TargetUsage = MFX_TARGETUSAGE_BEST_QUALITY;
 	//conf.RateControlMethod = MFX_RATECONTROL_VBR;
 	conf.mfx.TargetKbps = 10466;
 
@@ -88,7 +81,7 @@ void ConfigureEncodeParams(mfxVideoParam & conf) {
 	conf.mfx.FrameInfo.FourCC = MFX_FOURCC_NV12;
 	conf.mfx.FrameInfo.PicStruct = MFX_PICSTRUCT_PROGRESSIVE;
 	conf.mfx.FrameInfo.ChromaFormat = MFX_CHROMAFORMAT_YUV420;
-	conf.mfx.FrameInfo.FrameRateExtN = 1;
+	conf.mfx.FrameInfo.FrameRateExtN = 4; // FrameRate is N/D, 4 is the minimal for ffplay
 	conf.mfx.FrameInfo.FrameRateExtD = 1;
 }
 
@@ -115,7 +108,7 @@ mfxFrameSurface1** CreateSurfaces(mfxU16 suggestedFrame, mfxU16 width, mfxU16 he
 //static const mfxPluginUID  MFX_PLUGINID_CAPTURE_HW =	  { { 0x22, 0xd6, 0x2c, 0x07, 0xe6, 0x72, 0x40, 0x8f, 0xbb, 0x4c, 0xc2, 0x0e, 0xd7, 0xa0, 0x53, 0xe4 } };
 void Intel() {
 	mfxSession session; 
-	mfxVersion version{ 17,1 };
+	mfxVersion version{ 23,1 };
 	auto res = MFXInit(MFX_IMPL_HARDWARE, &version, &session);
 	res = MFXVideoUSER_Load(session, &MFX_PLUGINID_CAPTURE_HW, version.Major); //&MFX_PLUGINID_CAPTURE_HW
 	res = MFXVideoUSER_Load(session, &MFX_PLUGINID_H264LA_HW, version.Major); //&MFX_PLUGINTYPE_VIDEO_ENCODE
@@ -167,27 +160,36 @@ void Intel() {
 		if (res==MFX_ERR_NONE) {   
 			//res = MFXVideoCORE_SyncOperation(session, syncp, -1);
 			//ProcessFrame(outSurface);
-			res = MFXVideoENCODE_EncodeFrameAsync(session, NULL, outSurface, &stream, &syncpEncode);
-			if (res != MFX_ERR_MORE_DATA) {
-				
-				res = MFXVideoCORE_SyncOperation(session, syncpEncode, -1);
-				
-				std::ofstream outputFile;
-				outputFile.open("d:\\Idei\\POC\\ScreenCapture\\x64\\Debug\\res.h264", std::ofstream::binary);
-				if (!outputFile.fail()) {
-					outputFile.write(reinterpret_cast<char*>( stream.Data), stream.DataLength);
+
+			mfxStatus encodeStat = MFX_ERR_MORE_DATA;
+			/*while (encodeStat != MFX_ERR_NONE) */{
+				res = MFXVideoENCODE_EncodeFrameAsync(session, NULL, outSurface, &stream, &syncpEncode);
+				//no more frame
+				res = MFXVideoENCODE_EncodeFrameAsync(session, NULL, NULL, &stream, &syncpEncode);
+				encodeStat = res;
+				if (res != MFX_ERR_MORE_DATA) {
+
+					res = MFXVideoCORE_SyncOperation(session, syncpEncode, -1);
+
+					std::ofstream outputFile;
+					outputFile.open("d:\\Idei\\POC\\ScreenCapture\\x64\\Debug\\res.h264", std::ofstream::binary);
+					if (!outputFile.fail()) {
+						outputFile.write(reinterpret_cast<char*>(stream.Data), stream.DataLength);
+					}
+					else {
+						std::cout << strerror(errno) << std::endl;
+					}
+					outputFile.close();
+					idx = 0;
+					break;
 				}
-				else {
-					std::cout << strerror(errno) << std::endl;
-				}
-				outputFile.close();
-				idx = 0;
 			}
 		}
 
-		std::this_thread::sleep_for(std::chrono::seconds(1));
+		std::this_thread::sleep_for(std::chrono::seconds(10));
 	} 
 	MFXVideoDECODE_Close(session); 
+	MFXVideoENCODE_Close(session);
 	//free_pool_of_frame_surfaces(); 
 	MFXClose(session);
 }
